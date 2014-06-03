8394b7a4d07d63c9a7d0bee7866258eb6d78c4d5
Diego Lago <diego.lago.gonzalez@gmail.com>
2014-06-03 13:04:29 +0200

    Add my old codding style guide.

---
+++ es/guia-estilo/scripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$("div#contents").jqTOC({
+		tocWidth: 220,
+		tocTitle: 'Índice de Contenido',
+		tocStart: 1,
+		tocEnd  : 4,
+		tocContainer : 'toc_container',
+		tocAutoClose : false,
+		tocShowOnClick : false,
+		tocTopLink   : ''
+	});
+	 
+});
